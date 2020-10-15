43522a1c2a5cfb426682acff0dd07a09d7906729
Lorenzo SAMMATRICE <lsammatrice@lerebours.org>
2020-10-15 20:18:25 +0200

    Ajoutez des fichiers projet.

---
+++ MetierDico/Commercial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetierDico
+{
+    class Commercial
+    {
+        private string nomCommercial;
+        private List<Vente> lesVentes;
+
+        public Commercial(string unNom)
+        {
+            NomCommercial = unNom;
+            LesVentes = new List<Vente>();
+        }
+
+        public string NomCommercial { get => nomCommercial; set => nomCommercial = value; }
+        public List<Vente> LesVentes { get => lesVentes; set => lesVentes = value; }
+
+        public void AjouterVente(Vente uneVente)
+        {
+            LesVentes.Add(uneVente);
+        }
+    }
+}
---
+++ MetierDico/Vente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MetierDico
+{
+    public class Vente
+    {
+        private string nomClient;
+        private int montant;
+
+        public Vente(string unNom, int unMontant)
+        {
+            NomClient = unNom;
+            Montant = unMontant;
+        }
+
+        public string NomClient { get => nomClient; set => nomClient = value; }
+        public int Montant { get => montant; set => montant = value; }
+    }
+}
